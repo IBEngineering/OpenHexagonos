0f72f0d3835dc9f422b0790c225157f1b6f7f132
MisterCavespider <cavespider12@gmail.com>
2016-07-23 21:23:49 +0200

    Fixed a little bit at Main.

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -11,7 +11,7 @@ import com.jme3.scene.Spatial;
 import com.jme3.system.AppSettings;
 import nl.ibe.hex.game.HexGame;
 import nl.ibe.hex.game.HexPlayer;
-import nl.ibe.hex.view.ModelSupplier;
+import nl.ibe.hex.supply.ModelSupplier;
 import nl.ibe.hex.view.View;
 
 /**
@@ -51,8 +51,6 @@ public class Main extends SimpleApplication {
         View v = new View(this);
         g.register(v);
         v.construct();
-        
-        testingCase();
     }
     
     public void initMappings() {
@@ -70,18 +68,5 @@ public class Main extends SimpleApplication {
         Material mat = new Material(shortcut, "Common/MatDefs/Misc/Unshaded.j3md");
         mat.setColor("Color", color);
         return mat;
-    }
-
-    private void testingCase() {
-        for (int i = 0; i < 5; i++) {
-            Spatial s = ModelSupplier.getBacterium();
-            s.setLocalTranslation(0, 2 + i, 0);
-            rootNode.attachChild(s);
-        }
-        for (int i = 0; i < 5; i++) {
-            Spatial s = ModelSupplier.getCell();
-            s.setLocalTranslation(0, 2 + i, 5);
-            rootNode.attachChild(s);
-        }
     }
 }
